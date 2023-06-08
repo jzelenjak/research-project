6e1b90aaa273f2f9de0ea963a87e2c2880e1e422
Jegor Zelenjak <jzelenjak@proton.me>
2023-06-08 23:49:13 +0200

    Update get-paths.sh to include victims

--- get-paths.sh
+++ get-paths.sh
@@ -16,7 +16,15 @@ usage() {
 [[ $# -ne 1 ]] && { usage >&2 ; exit 1 ; }
 
 
+#"Victim: 10.0.0.81
+#DATA EXFILTRATION
+#http" [shape=doubleoctagon, style=filled, fillcolor=salmon];
 if [[ -f "$1" ]] && [[ "${1##*.}" == "dot" ]]; then
+    ag_name=$(grep -A 2 '^"Victim: [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+' "$1" | head -3 | tr '\n' '|' | sed 's/\[.*//g' | tr -d '"' | sed 's/Victim: //')
+    echo "$ag_name"
+    victim=$(sed 's/|.*$//' <<< "$ag_name")
+    #echo $victim
+
     sed 's/\]$/]KIRIL/g' "$1" |
         sed 's/}$/}KIRIL/' |
         tr -d '\r' |
@@ -30,8 +38,8 @@ if [[ -f "$1" ]] && [[ "${1##*.}" == "dot" ]]; then
         sed 's/ | ID: -\?[0-9]\+//g' |
         tr -d '"' |
         awk -F '\t' 'NF == 3 { print "\t" $0 } NF == 4 { print $0 }' |
-        awk -F '\t' 'BEGIN { current_attacker = ""; } $1 != "" { current_attacker = $1; print "\n" $0 } $1 == "" { print current_attacker "\t" $2 "\t" $3 "\t" $4 }' |
-        sed '1d'
+        awk -F '\t' 'BEGIN { current_attacker = ""; } $1 != "" { current_attacker = $1; print "\n" $1 "\t" "'"$victim"'" "\t" $2 "\t" $3 "\t" $4 } $1 == "" { print current_attacker "\t" "'"$victim"'" "\t" $2 "\t" $3 "\t" $4 }' #|
+        #sed '1d'
 elif [[ -d "$1" ]]; then
     find "$1" -type f -name '*.dot' | sed 's@^\(.*\)$@'"$0"' \1 ; echo -e "\\n"@' | sh
 else
